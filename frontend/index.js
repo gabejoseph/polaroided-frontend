56d912f8721e1eb7783a55bfc4943cdd78492740
gabejoseph <gabriel.joseph4@gmail.com>
2020-08-10 10:57:32 -0400

    adding in first fetch to index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+    fetchUsers()
 })
+
+const BASE_URL = "http://localhost:3000/"
+
+// read 
+
+function fetchUsers () {
+    fetch(`${BASE_URL}/users`)
+    .then(resp => resp.json())
+    .then(users => {
+        for (const user of users) {
+            console.log("rails object", user)
+            let u = new User(user.id, user.name, user.username, user.email)
+            console.log("javascript object", u)
+        }
+    })
+}
+
+// create
+
+
+// delete
---
+++ frontend/src/user.js
@@ -0,0 +1,11 @@
+class User {
+    constructor(id, name, username, email) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+    }
+
+    // instance method thats going to render the object to the dom
+    
+}
