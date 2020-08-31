b8b66564f784692b39a2cb8ba11bada0db515fcd
gabejoseph <gabriel.joseph4@gmail.com>
2020-08-30 23:30:25 -0400

    centered username card

--- frontend/src/user.js
+++ frontend/src/user.js
@@ -14,22 +14,20 @@ class User {
         const usersDiv = document.querySelector(".users-container")
         usersDiv.innerHTML += 
         `
-        <div class="mdc-card mdc-card--outlined">
-            <div class="eachUser row" id=${this.id}>
-                <div class="mdc-card mdc-card--outlined">
-                    <ul>
-                        <h3>Username: ${this.username}</h3>
-                        <li>${this.name} - ${this.email}</li>
-                    </ul>
-                    <button 
-                        id="delete-bttn" 
-                        data-id=${this.id} 
-                        onClick="deleteUser()"
-                        class="mdc-button foo-button">
-                        <div class="mdc-button__ripple"></div>
-                        Delete User
-                    </button>
-                </div>
+        <div class="eachUser mdc-card mdc-card--outlined" id=${this.id}>
+            <div class="user-data-container" >
+                <ul>
+                    <h3>Username: ${this.username}</h3>
+                    <li>${this.name} - ${this.email}</li>
+                </ul>
+                <button 
+                    id="delete-bttn" 
+                    data-id=${this.id} 
+                    onClick="deleteUser()"
+                    class="mdc-button foo-button">
+                    <div class="mdc-button__ripple"></div>
+                    Delete User
+                </button>
             </div>
         </div>
         `
