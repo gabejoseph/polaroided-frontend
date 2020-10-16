9bb795d2a358883d37c309d58a0937f7d69c2794
gabejoseph <gabriel.joseph4@gmail.com>
2020-10-16 15:58:59 -0400

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -31,6 +31,9 @@ function App() {
   }, [])
 
 
+  handleState = () => {
+
+  }
 
 
   return (
@@ -45,11 +48,7 @@ function App() {
 
 
       {
-        posts.map(post => photos.map(photo => 
-          {if (post.id === photo.user_id) {
-            console.log(post.id === photo.user_id)
-          }}
-        ))        
+        this.handleState()       
       }
 
       </div>
