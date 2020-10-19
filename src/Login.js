467209e4ef8c93dac461ce333092518f300d1b8c
gabejoseph <gabriel.joseph4@gmail.com>
2020-10-19 15:03:59 -0400

    adding in login and register

---
+++ src/Login.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+
+import { userActions } from '../actions/user.actions';
+
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // reset login status
+        this.props.logout();
+
+        this.state = {
+            username: '',
+            password: '',
+            submitted: false
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        this.setState({ submitted: true });
+        const { username, password } = this.state;
+        if (username && password) {
+            this.props.login(username, password);
+            this.props.history.push('/')
+        }
+    }
+
+    render() {
+        const { loggingIn } = this.props;
+        const { username, password, submitted } = this.state;
+        return (
+            <div>
+                <h2>Login</h2>
+                <form name="form" onSubmit={this.handleSubmit}>
+                    <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
+                        <TextField type="text" label="Username" htmlFor="username" className="form-control" name="username" value={username} onChange={this.handleChange} />
+                        {submitted && !username &&
+                            <div className="help-block">Username is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
+                        <TextField type="password" htmlFor="password" label="Password" className="form-control" name="password" value={password} onChange={this.handleChange} />
+                        {submitted && !password &&
+                            <div className="help-block">Password is required</div>
+                        }
+                    </div>
+                    <div className="form-group">
+                        <button className="btn btn-primary">Login</button>
+                        {loggingIn}
+                        <Button><Link to="/register" className="btn btn-link">Register</Link></Button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+function mapState(state) {
+    const { loggingIn } = state.authentication;
+    return { loggingIn };
+}
+
+const actionCreators = {
+    login: userActions.login,
+    logout: userActions.logout
+};
+
+export default connect(mapState, actionCreators)(Login);
---
+++ src/Register.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { makestyles } from '@material-ui/core/styles'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+
+import { userActions } from '../actions/user.actions'
+
+class Register extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            // name, email, username and password
+            user: {
+                name: '',
+                email: '',
+                username: '',
+                password: ''
+            },
+            submitted: false
+        };
+
+    }
+
+
+    handleChange(event) {
+        const { name, value } = event.target;
+        const { user } = this.state;
+        this.setState({
+            user: {
+                ...user,
+                [name]: value
+            }
+        });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+
+        this.setState({ submitted: true });
+        const { user } = this.state;
+        // user.name user.email user.username user.password
+        if (user.name && user.email && user.username && user.password) {
+            this.props.register(user);
+            this.props.login(user.username, user.password);
+            this.props.history.push('/');
+        }
+    }
+
+
+
+
+    render() {
+        const { registering  } = this.props;
+        const { user, submitted } = this.state;
+
+        return (
+            <div className="col-md-6 col-md-offset-3">
+                <h2>Register</h2>
+                <form name="form" onSubmit={(event) => this.handleSubmit(event)}>
+                    <div className={'form-group' + (submitted && !user.name ? ' has-error' : '')}>
+                        <TextField label="Name" htmlFor="name" type="text" className="form-control" name="name" value={user.name} onChange={(event) => this.handleChange(event)} />
+                        {submitted && !user.name &&
+                            <div className="help-block">Name is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !user.email ? ' has-error' : '')}>
+                        <TextField type="text" label="Email" htmlFor="email" className="form-control" name="email" value={user.email} onChange={(event) => this.handleChange(event)} />
+                        {submitted && !user.email &&
+                            <div className="help-block">Email is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !user.username ? ' has-error' : '')}>
+                        <TextField htmlFor="username" label="Username" type="text" className="form-control" name="username" value={user.username} onChange={(event) => this.handleChange(event)} />
+                        {submitted && !user.username &&
+                            <div className="help-block">Username is required</div>
+                        }
+                    </div>
+                    <div className={'form-group' + (submitted && !user.password ? ' has-error' : '')}>
+                        <TextField htmlFor="password" label="Password" type="password" className="form-control" name="password" value={user.password} onChange={(event) => this.handleChange(event)} />
+                        {submitted && !user.password &&
+                            <div className="help-block">Password is required</div>
+                        }
+                    </div>
+                    <div className="form-group">
+                        <button className="btn btn-primary">Register</button>
+                        {registering && <strong>Loading ... </strong>}
+                        <Button><Link to="/" >Cancel</Link></Button>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+function mapState(state) {
+    const { registering } = state.registration;
+    return { registering };
+}
+
+const actionCreators = {
+    register: userActions.register,
+    login: userActions.login
+}
+
+export default connect(mapState, actionCreators)(Register);
