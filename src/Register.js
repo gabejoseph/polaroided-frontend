6f0d3369e2a1306e29c935e34714588f08f65f5d
gabejoseph <gabriel.joseph4@gmail.com>
2020-10-19 15:51:46 -0400

    added in registration, login and logout functions.  Yet to be implemented

--- src/Register.js
+++ src/Register.js
@@ -106,3 +106,55 @@ class Register extends React.Component {
 // }
 
 export default Register
+
+// function register(user) {
+//     const requestOptions = {
+//         method: 'POST',
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(user)
+//     };
+
+//     return fetch(`${BASE_URL}/registrations`, requestOptions).then(handleResponse);
+// }
+
+// function update(user) {
+//     const requestOptions = {
+//         method: 'PATCH',
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(user)
+//     };
+
+//     return fetch(`${BASE_URL}/users/${user.id}`, requestOptions).then(handleResponse);;
+// }
+
+// // prefixed function name with underscore because delete is a reserved word in javascript
+// function _delete(id) {
+//     const requestOptions = {
+//         method: 'DELETE'
+//     };
+
+//     return fetch(`${BASE_URL}/users/${id}`, requestOptions).then(handleResponse);
+// }
+
+// function handleResponse(response) {
+//     return response.text().then(text => {
+//         const data = text && JSON.parse(text);
+//         if (!response.ok) {
+//             if (response.status === 401) {
+//                 // auto logout if 401 response returned from api
+//                 logout();
+//                 // location.reload(true);
+//             }
+//             const error = (data && data.message) || response.statusText;
+//             return Promise.reject(error);
+//         }
+//         console.log(data)
+//         return data;
+//     });
+// }
